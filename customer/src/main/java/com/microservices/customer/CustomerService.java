f60504f186dd9fadf44f92819172d6ae2329e9d1
Igor Nosovskyi <igor.nosovskyi@external.flixbus.com>
2022-06-05 21:59:28 +0300

    add notification microservice

--- customer/src/main/java/com/microservices/customer/CustomerService.java
+++ customer/src/main/java/com/microservices/customer/CustomerService.java
@@ -2,16 +2,17 @@ package com.microservices.customer;
 
 import com.microservices.clients.fraud.FraudCheckResponse;
 import com.microservices.clients.fraud.FraudClient;
+import com.microservices.clients.notification.NotificationClient;
+import com.microservices.clients.notification.NotificationRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
-    private final RestTemplate restTemplate;
     private final CustomerRepository customerRepository;
     private final FraudClient fraudClient;
+    private final NotificationClient notificationClient;
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -26,5 +27,12 @@ public class CustomerService {
         if(fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("Fraud");
         }
+       notificationClient.sendNotification(
+               new NotificationRequest(
+                       customer.getId(),
+                       customer.getEmail(),
+                       String.format("Welcome %s %s", customer.getFirstName(), customer.getLastName())
+               )
+       );
     }
 }
